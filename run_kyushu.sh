b3d446e765a635fd8145a77d797aad628a2f1f7a
Atsushi Ishikawa <ishikawasuteado@gmail.com>
2018-02-24 14:46:32 +0900

    asdf

--- run_kyushu.sh
+++ run_kyushu.sh
@@ -2,12 +2,12 @@
 #PJM -L "rscunit=ito-a"
 #PJM -L "rscgrp=ito-a-oc170117"
 #PJM -L "vnode=1"
-#PJM -L "vnode-core=12"
+#PJM -L "vnode-core=18"
 #PJM -L "elapse=10:00:00"
 #PJM -j
 #PJM -X
 
-NUM_PROCS=12
+NUM_PROCS=18
 
 module load intel/2017
 
@@ -22,6 +22,10 @@ export I_MPI_HYDRA_BOOTSTRAP=rsh
 export I_MPI_HYDRA_BOOTSTRAP_EXEC=/bin/pjrsh
 export I_MPI_HYDRA_HOST_FILE=${PJM_O_NODEINF}
 
+echo $INP1 $INP2 $INP3
+
 # mpiexec.hydra -n $NUM_PROCS $PRG > $OUT
-python adsorption.py $INP 1> std_${INP}_ads.out 2> err_${INP}_ads.out
+# python adsorption.py $INP 1> std_${INP}_ads.out 2> err_${INP}_ads.out
+# python adsorption.py ${INP} 1> std_${INP}_ads.out 2> err_${INP}_ads.out
+python adsorption.py ${INP1} ${INP2} ${INP3} 1> std_${INP1}_${INP2}_${INP3}_ads.out 2> err_${INP1}_${INP2}_${INP3}_ads.out
 
