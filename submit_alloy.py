f98561274026162d75fc5b759990cc8c3f2115a8
Atsushi Ishikawa <ishikawasuteado@gmail.com>
2018-02-27 17:51:54 +0900

    from kyushu

--- submit_alloy.py
+++ submit_alloy.py
@@ -16,7 +16,6 @@ for i in range(0,len(dict)):
 		element2 = dict[str(j)]["element"]
 		# for comp1 in range(0,150,50): # start, end, diff
 		comp1 = 50
-		print element1,element2,comp1
 		# command = "qsub run_alloy.sh " + str(element1) + " " + str(element2) + " " + str(comp1) # gridengine
 		command = "pjsub -x \"INP1={0}\" -x \"INP2={1}\" -x \"INP3={2}\" run_kyushu.sh".format(element1,element2,comp1) # PBS
 		os.system(command)
