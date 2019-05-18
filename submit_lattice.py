81a15f46c0d0febb42de2931921326800172f2e6
Atsushi Ishikawa <ishikawasuteado@gmail.com>
2019-05-18 13:59:35 +0900

    from mac

---
+++ submit_lattice.py
@@ -0,0 +1,21 @@
+import os,sys
+#
+# lattice constants takne from 
+# http://periodictable.com/Properties/A/LatticeConstants.html
+#
+dict = {
+  "Rh" : {"lattice" : "fcc" , "a0" : 3.803}, 
+  "Pd" : {"lattice" : "fcc" , "a0" : 3.891}, 
+  "Ir" : {"lattice" : "fcc" , "a0" : 3.839}, 
+  "Pt" : {"lattice" : "fcc" , "a0" : 3.924},
+  "Cu" : {"lattice" : "fcc" , "a0" : 3.615}, 
+  "Ag" : {"lattice" : "fcc" , "a0" : 4.085}, 
+  "Au" : {"lattice" : "fcc" , "a0" : 4.078} 
+}
+
+for element in dict:
+	lattice = dict[element]["lattice"]
+	a0      = dict[element]["a0"]
+	command = "qsub run_lattice.sh " + str(element) + " " + str(lattice) + " " + str(a0)
+	os.system(command)
+
