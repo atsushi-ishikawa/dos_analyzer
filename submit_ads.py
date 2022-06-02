6549d35efe40886945d4023bf65ca89b8caa0d60
ISHIKAWA Atsushi <ishikawa.atsushi@nims.go.jp>
2022-06-02 17:21:36 +0900

    Changes

--- submit_ads.py
+++ submit_ads.py
@@ -1,61 +1,62 @@
-import os,sys
+import os
+import sys
 
 # pure metals
 # note: no need to do separate calculation for pure metals when making alloy-including database,
 #       just set "First=True" flag in alloy part
 
 alloy = True
-env = "hokudai" # or "hokudai"
+env = "hokudai"  # or "hokudai"
 
 dict = {
-  "0"  : {"element" : "Rh" , "lattice" : "fcc" },
-  "1"  : {"element" : "Pd" , "lattice" : "fcc" }, 
-  "2"  : {"element" : "Ir" , "lattice" : "fcc" }, 
-  "3"  : {"element" : "Pt" , "lattice" : "fcc" }, 
-  "4"  : {"element" : "Cu" , "lattice" : "fcc" }, 
-  "5"  : {"element" : "Ag" , "lattice" : "fcc" }, 
-  "6"  : {"element" : "Au" , "lattice" : "fcc" }
+  "0": {"element": "Rh", "lattice": "fcc"},
+  "1": {"element": "Pd", "lattice": "fcc"},
+  "2": {"element": "Ir", "lattice": "fcc"},
+  "3": {"element": "Pt", "lattice": "fcc"},
+  "4": {"element": "Cu", "lattice": "fcc"},
+  "5": {"element": "Ag", "lattice": "fcc"},
+  "6": {"element": "Au", "lattice": "fcc"}
 }
 
 first = True
 # alloy
 if alloy:
-	for i in range(0,len(dict)):
-		element1 = dict[str(i)]["element"]
-		for j in range(i+1,len(dict)):
-			element2 = dict[str(j)]["element"]
+    for i in range(0, len(dict)):
+        elm1 = dict[str(i)]["element"]
+        for j in range(i+1, len(dict)):
+            elm2 = dict[str(j)]["element"]
 
-			# --- change by 50% --> gives 21 systems w.o. pure metals
-			#for comp1 in range(50,100,50): # start, end, diff
+            # --- change by 50% --> gives 21 systems w.o. pure metals
+            #for cmp1 in range(50,100,50): # start, end, diff
 
-			# --- change by 25% --> gives 63 systems w.o. pure metals
-			#for comp1 in range(25,100,25): # start, end, diff
+            # --- change by 25% --> gives 63 systems w.o. pure metals
+            #for cmp1 in range(25,100,25): # start, end, diff
 
-			# --- change by 10%
-			#first = False
-			#for comp1 in range(10,50,10):  # start, end, diff # part1
-			#for comp1 in range(50,100,10): # start, end, diff # part2
+            # --- change by 10%
+            #first = False
+            #for cmp1 in range(10,50,10):  # start, end, diff # part1
+            #for cmp1 in range(50,100,10): # start, end, diff # part2
 
-			# --- change by 5%
-			first = True
-			for comp1 in range(5,25,5):   # start, end, diff # part1
-			#for comp1 in range(25,50,5):  # start, end, diff # part2
-			#for comp1 in range(50,75,5):  # start, end, diff # part3
-			#for comp1 in range(75,100,5): # start, end, diff # part4
+            # --- change by 5%
+            first = True
+            # for cmp1 in range(25,50,5):  # start, end, diff # part2
+            # for cmp1 in range(50,75,5):  # start, end, diff # part3
+            # for cmp1 in range(75,100,5): # start, end, diff # part4
 
-				if env=="ito":
-					command = "pjsub -x \"INP1={0}\" -x \"INP2={1}\" -x \"INP3={2}\" run_kyushu.sh".format(element1,element2,comp1) # PBS
-				elif env=="hokudai":
-					command = "pjsub -x \"INP1={0}\" -x \"INP2={1}\" -x \"INP3={2}\" run_vasp.sh".format(element1,element2,comp1) # PBS
-				os.system(command)
+            for cmp1 in range(5, 25, 5):   # start, end, diff # part1
+                if env == "ito":
+                    cmd = "pjsub -x \"INP1={0}\" -x \"INP2={1}\" -x \"INP3={2}\" run_kyushu.sh".format(elm1, elm2, cmp1)
+                elif env == "hokudai":
+                    cmd = "pjsub -x \"INP1={0}\" -x \"INP2={1}\" -x \"INP3={2}\" run_vasp.sh".format(elm1, elm2, cmp1)
+
+                os.system(command)
 
 # do pure metal if the loop is first
 if first:
-	for i in range(0,len(dict)):
-		element = dict[str(i)]["element"]
-		if env=="ito":
-			command = "pjsub -x \"INP1={0}\" run_kyushu.sh".format(element)
-		elif env=="hokudai":
-			command = "pjsub -x \"INP1={0}\" run_vasp.sh".format(element)
-		os.system(command)
-
+    for i in range(0,len(dict)):
+        element = dict[str(i)]["element"]
+        if env == "ito":
+            command = "pjsub -x \"INP1={0}\" run_kyushu.sh".format(element)
+        elif env == "hokudai":
+            command = "pjsub -x \"INP1={0}\" run_vasp.sh".format(element)
+        os.system(command)
--- vasptools.py
+++ vasptools.py
@@ -1,172 +1,172 @@
 def lattice_info_guess(bulk):
-	from ase import Atoms
-	#
-	# dict for already-known cases
-	#
-	dict = {
-		"Rh": {"lattice": "fcc", "a0": 3.803},
-		"Pd": {"lattice": "fcc", "a0": 3.891},
-		"Ir": {"lattice": "fcc", "a0": 3.839},
-		"Pt": {"lattice": "fcc", "a0": 3.924},
-		"Cu": {"lattice": "fcc", "a0": 3.615},
-		"Ag": {"lattice": "fcc", "a0": 4.085},
-		"Au": {"lattice": "fcc", "a0": 4.078}
-	}
-	element = bulk.get_chemical_symbols()[0]
-	if element in dict:
-		print("I know this.")
-		lattice = dict[element]["lattice"]
-		a0 = dict[element]["a0"]
-	else:
-		print("I don't know this.")
-		lattice = "fcc"
-		a0 = 4.0
+    from ase import Atoms
+    #
+    # dict for already-known cases
+    #
+    dict = {
+        "Rh": {"lattice": "fcc", "a0": 3.803},
+        "Pd": {"lattice": "fcc", "a0": 3.891},
+        "Ir": {"lattice": "fcc", "a0": 3.839},
+        "Pt": {"lattice": "fcc", "a0": 3.924},
+        "Cu": {"lattice": "fcc", "a0": 3.615},
+        "Ag": {"lattice": "fcc", "a0": 4.085},
+        "Au": {"lattice": "fcc", "a0": 4.078}
+    }
+    element = bulk.get_chemical_symbols()[0]
+    if element in dict:
+        print("I know this.")
+        lattice = dict[element]["lattice"]
+        a0 = dict[element]["a0"]
+    else:
+        print("I don't know this.")
+        lattice = "fcc"
+        a0 = 4.0
 
-	return lattice, a0
+    return lattice, a0
 
 
 def make_bulk(element1, element2=None, comp1=100, lattice="fcc", a0=4.0, repeat=1):
-	from ase import Atoms
-	from ase.build import bulk
-	import numpy as np
-	from ase.visualize import view
+    from ase import Atoms
+    from ase.build import bulk
+    import numpy as np
+    from ase.visualize import view
 
-	# to fractional
-	if element2 is not None:
-		comp2  = 100.0 - comp1
-		comp2 /= 100.0
+    # to fractional
+    if element2 is not None:
+        comp2  = 100.0 - comp1
+        comp2 /= 100.0
 
-	# form bulk first
-	bulk = bulk(element1, lattice, a=a0, cubic=True)
-	bulk = bulk.repeat(repeat)  # for consistency with alloys
+    # form bulk first
+    bulk = bulk(element1, lattice, a=a0, cubic=True)
+    bulk = bulk.repeat(repeat)  # for consistency with alloys
 
-	if element2 is not None:
+    if element2 is not None:
 
-		# make alloy if needed
-		natom_tot = len(bulk.get_atomic_numbers())
-		natom2    = int(comp2 * natom_tot)
+        # make alloy if needed
+        natom_tot = len(bulk.get_atomic_numbers())
+        natom2    = int(comp2 * natom_tot)
 
-		# replace atoms in bulk randomly
-		list = bulk.get_chemical_symbols()
+        # replace atoms in bulk randomly
+        list = bulk.get_chemical_symbols()
 
-		#np.random.seed(1)# set random seed for reproducability
-		np.random.seed()
+        #np.random.seed(1)# set random seed for reproducability
+        np.random.seed()
 
-		replace_list = np.random.choice(len(list), size=natom2, replace=False)
-		for i in replace_list:
-			list[i] = element2
+        replace_list = np.random.choice(len(list), size=natom2, replace=False)
+        for i in replace_list:
+            list[i] = element2
 
-		bulk.set_chemical_symbols(list)
+        bulk.set_chemical_symbols(list)
 
-	return bulk
+    return bulk
 
 
 def optimize_lattice_constant(bulk, lattice="fcc", a0=4.0, xc="PBEsol",
-							  encut=400, ediff=1.0e-5, ediffg=1.0e-6, npar=1, nsim=1):
-	""" 
-	function to do bulk optimization
-	"""
-	from ase import Atoms
-	from ase.calculators.vasp import Vasp
-	import os
-	import shutil
+                              encut=400, ediff=1.0e-5, ediffg=1.0e-6, npar=1, nsim=1):
+    """
+    function to do bulk optimization
+    """
+    from ase import Atoms
+    from ase.calculators.vasp import Vasp
+    import os
+    import shutil
 
-	# directry things
-	cudir   = os.getcwd()
-	workdir = os.path.join(cudir, "lattice_opt")
-	os.makedirs(workdir)
-	os.chdir(workdir)
-	os.system("mkdir work_bulk")
+    # directry things
+    cudir   = os.getcwd()
+    workdir = os.path.join(cudir, "lattice_opt")
+    os.makedirs(workdir)
+    os.chdir(workdir)
+    os.system("mkdir work_bulk")
 
-	# compuational condition for Vasp
-	prec   = "normal"
-	potim  = 0.1
-	nelmin = 5
-	kpts   = [3, 3, 3]
-	gamma  = True
-	nsw    = 200
-	isif   = 6  # or 6---freezing ions
+    # compuational condition for Vasp
+    prec   = "normal"
+    potim  = 0.1
+    nelmin = 5
+    kpts   = [3, 3, 3]
+    gamma  = True
+    nsw    = 200
+    isif   = 6  # or 6---freezing ions
 
-	xc = xc.lower()
-	if xc == "pbe" or xc == "pbesol" or xc == "rpbe":
-		pp = "pbe"
-	elif xc == "pw91":
-		pp = "pw91"
-	elif xc == "lda":
-		pp = "lda"
-	else:
-		print("xc error")
+    xc = xc.lower()
+    if xc == "pbe" or xc == "pbesol" or xc == "rpbe":
+        pp = "pbe"
+    elif xc == "pw91":
+        pp = "pw91"
+    elif xc == "lda":
+        pp = "lda"
+    else:
+        print("xc error")
 
-	calc = Vasp(prec=prec, xc=xc, pp=pp, ispin=1,
-				ismear=1, sigma=0.2, isif=isif, nelmin=nelmin, encut=encut,
-				ibrion=2, nsw=nsw, potim=potim, ediff=ediff, ediffg=ediffg,
-				kpts=kpts, gamma=gamma, isym=0, npar=npar, nsim=nsim, lreal=False)
+    calc = Vasp(prec=prec, xc=xc, pp=pp, ispin=1,
+                ismear=1, sigma=0.2, isif=isif, nelmin=nelmin, encut=encut,
+                ibrion=2, nsw=nsw, potim=potim, ediff=ediff, ediffg=ediffg,
+                kpts=kpts, gamma=gamma, isym=0, npar=npar, nsim=nsim, lreal=False)
 
-	bulk.set_calculator(calc)
-	bulk.get_potential_energy()
+    bulk.set_calculator(calc)
+    bulk.get_potential_energy()
 
-	os.chdir(cudir)
-	shutil.rmtree(workdir)
+    os.chdir(cudir)
+    shutil.rmtree(workdir)
 
 
 def sort_atoms_by_z(atoms):
-	from ase import Atoms, Atom
-	import numpy as np
+    from ase import Atoms, Atom
+    import numpy as np
 
-	# keep information for original Atoms
-	tags = atoms.get_tags()
-	pbc  = atoms.get_pbc()
-	cell = atoms.get_cell()
+    # keep information for original Atoms
+    tags = atoms.get_tags()
+    pbc  = atoms.get_pbc()
+    cell = atoms.get_cell()
 
-	dtype = [("idx", int), ("z", float)]
-	zlist = np.array([], dtype=dtype)
+    dtype = [("idx", int), ("z", float)]
+    zlist = np.array([], dtype=dtype)
 
-	for idx, atom in enumerate(atoms):
-		tmp = np.array([(idx, atom.z)], dtype=dtype)
-		zlist = np.append(zlist, tmp)
+    for idx, atom in enumerate(atoms):
+        tmp = np.array([(idx, atom.z)], dtype=dtype)
+        zlist = np.append(zlist, tmp)
 
-	zlist = np.sort(zlist, order="z")
+    zlist = np.sort(zlist, order="z")
 
-	newatoms = Atoms()
+    newatoms = Atoms()
 
-	for i in zlist:
-		idx = i[0]
-		newatoms.append(atoms[idx])
+    for i in zlist:
+        idx = i[0]
+        newatoms.append(atoms[idx])
 
-	# restore
-	newatoms.set_tags(tags)
-	newatoms.set_pbc(pbc)
-	newatoms.set_cell(cell)
+    # restore
+    newatoms.set_tags(tags)
+    newatoms.set_pbc(pbc)
+    newatoms.set_cell(cell)
 
-	return newatoms
+    return newatoms
 
 
 def make_lobsterin():
-	"""
-	make input file for lobster (lobsterin) for alloy systems
-	"""
-	f = open("lobsterin", "w")
-	str = ["COHPstartEnergy -20", "COHPendEnergy 20",
-		   "basisSet pbeVaspFit2015", "includeOrbitals spd", "cohpbetween atom 1 and atom 2"]
-	str = " \n".join(str)
-	f.write(str)
-	f.close()
+    """
+    make input file for lobster (lobsterin) for alloy systems
+    """
+    f = open("lobsterin", "w")
+    str = ["COHPstartEnergy -20", "COHPendEnergy 20",
+           "basisSet pbeVaspFit2015", "includeOrbitals spd", "cohpbetween atom 1 and atom 2"]
+    str = " \n".join(str)
+    f.write(str)
+    f.close()
 
 
 def read_cohp(cohpfile="COHPCAR.lobster"):
-	f = open(cohpfile, "r")
-	f.readline()  # skip first
-	ndos = int(f.readline().split()[2])
+    f = open(cohpfile, "r")
+    f.readline()  # skip first
+    ndos = int(f.readline().split()[2])
 
-	[f.readline() for i in range(2)]
+    [f.readline() for i in range(2)]
 
-	ene  = []
-	cohp = []
+    ene  = []
+    cohp = []
 
-	for i in range(ndos):
-		tmp = f.readline().split()
-		ene.append(float(tmp[0]))
-		cohp.append(float(tmp[1]))
-	
-	f.close()
-	return ene, cohp
+    for i in range(ndos):
+        tmp = f.readline().split()
+        ene.append(float(tmp[0]))
+        cohp.append(float(tmp[1]))
+
+    f.close()
+    return ene, cohp
